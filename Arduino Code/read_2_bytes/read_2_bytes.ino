1821663036f7a3af503f84221a4b499f973bfb60
Chance Reimer <chance189@knights.ucf.edu>
2020-03-22 23:25:40 -0400

    Cleaned Arduino Code

--- Arduino Code/read_2_bytes/read_2_bytes.ino
+++ Arduino Code/read_2_bytes/read_2_bytes.ino
@@ -5,10 +5,16 @@ crc_8 crc8;
 Servo panServo;  // create servo object to control a servo
 Servo tiltServo;
 
+/*** vars for Servo ***/
 int tiltPos = 110;    // variable to store the servo position
-int panPos = 90;    // variable to store the servo position
+int panPos = 90;      // variable to store the servo position
 int laserPin = 13;
-byte inBytes [3];
+
+/*** vars for serial state ***/
+int state, cntr = 0;
+bool read_in_prog;        // Denotes that a read is currently being performed
+byte inBytes [3];   
+int in_byte;
 int inByte0, inByte1;
 
 void setup() {
@@ -28,7 +34,19 @@ void setup() {
 }
 
 void loop() {
-  
+  handle_serial_info();
+}
+
+/***
+ * Func: handle_serial_info
+ * params: null
+ * Purpose: handles information from serial as it becomes available (on a byte per byte basis)
+ *          function handles state in a rudimentary fashion through switch case. Handle each byte
+ *          input as it comes through, and update variables as necessary once CRC checks out.
+ *         
+ */
+void handle_serial_info()
+{
   if (Serial.available()) {
 
     while (Serial.available() < 3) {} // Wait 'till there are 3 Bytes waiting
@@ -41,59 +59,25 @@ void loop() {
     if(crc8.get_crc() != inBytes[2])  //CRC Mismatch
     {
       Serial.println("BAD CRC MATCH!!! RECV: " + String(inBytes[2], HEX));
+      input_buffer_flush();
     }
     else {
-      Serial.println("CRC MATCH! " + String(inBytes[2], HEX));
+      //Serial.println("CRC MATCH! " + String(inBytes[2], HEX));
       // Print out our new positions
-      if((inBytes[0] >> 7) & 1u) {
-        Serial.println("DEBUGGIN BYTE0: " + String(inBytes[0], BIN));
-        inByte0 = -1*((~(inBytes[0])+ 1)&0xFF);
-        Serial.println("DEBUGGIN BYTE0 AFTER: " + String(inByte0, BIN));
-      }
-      else {
-        inByte0 = inBytes[0]&0xFF;
-      }
-  
-      if((inBytes[1] >> 7) & 1u) {
-        Serial.println("DEBUGGIN BYTE1: " + String(inBytes[1], BIN));
-        inByte1 = -1*((~(inBytes[1]) + 1)&0xFF);
-        Serial.println("DEBUGGIN BYTE1 AFTER: " + String(inByte1, BIN));
-      }
-      else {
-        inByte1 = inBytes[1]&0xFF;
-      }
+      inByte0 = conv_unsignedbyte_signedint(inBytes[0]);
+      inByte1 = conv_unsignedbyte_signedint(inBytes[1]);
       
       panPos = panPos - inByte0;
       tiltPos = tiltPos + inByte1;
       // Subtract 100 from each number we got and update the new positions
       //panPos = panPos - (int(inBytes[0]));
       //tiltPos = tiltPos + (int(inBytes[1]));
-      Serial.println("RECEIVED BYTE 0: " + String(inByte0, DEC));
-      Serial.println("RECEIVED BYTE 1: " + String(inByte1, DEC));
-      Serial.println("New panPos: "      + String(panPos, DEC));
-      Serial.println("New tiltPos: "     + String(tiltPos, DEC));
-  
-      // Check if we need to FIRE!!
-      if (inBytes[0] == 200 & inBytes[1] == 200) {
-  
-        // FIRE THE BASS CANNON
-        Serial.println("FIRE");
-        digitalWrite(laserPin, HIGH);
-        delay(1000);
-        digitalWrite(laserPin, LOW);
-  
-        // Set the servos back to the ready state
-        tiltPos = 110;
-        panPos = 90;
-        tiltServo.write(tiltPos);
-        panServo.write(panPos);
-  
-        // Send confirmation
-        Serial.write("g");
-      }
-  
+      //Serial.println("RECEIVED BYTE 0: " + String(inByte0, DEC));
+      //Serial.println("RECEIVED BYTE 1: " + String(inByte1, DEC));
+      //Serial.println("New panPos: "      + String(panPos, DEC));
+      //Serial.println("New tiltPos: "     + String(tiltPos, DEC));
+      
       // Move servos to next position
-  
       Serial.println("Panning by: " + String(panPos, DEC));
       tiltServo.write(tiltPos);
       Serial.println("Tilting to: " + String(tiltPos, DEC));
@@ -102,5 +86,26 @@ void loop() {
 
     // Send Ack
     Serial.write("*");
+  }
+}
+
+/***
+ * Will convert input byte to an int and return it
+ * Function assumes that input byte is free of errors, therefore put this after CRC
+ */
+int conv_unsignedbyte_signedint(byte unsigned_byte) {
+  int signed_byte;
+  if((unsigned_byte >> 7) & 1u) {
+    signed_byte = -1*((~(unsigned_byte) + 1)&0xFF);
+  }
+  else {
+    signed_byte = unsigned_byte&0xFF;
+  }
+  return signed_byte;
+}
+
+void input_buffer_flush() {
+  while(Serial.available()) {
+    Serial.read();
   }
 }
